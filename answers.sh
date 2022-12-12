c21f0c5fe69f97b5fc42791e2fc4f34a609c9a7d
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-12 19:03:12 +0200

    completed joins

--- answers.sh
+++ answers.sh
@@ -44,3 +44,10 @@ WHERE name = 'Layal';
 
 # 4 :
 DELETE FROM students WHERE name="Layal" ;
+
+# joins :
+SELECT employees.Name , employees.Company , companies.Date FROM employees INNER JOIN companies ON companies.Name=employees.Company ;
+
+SELECT employees.name FROM employees INNER JOIN companies ON employees.Company=companies.Name WHERE companies.Date<2000;
+
+SELECT companies.Name FROM companies INNER JOIN employees ON employees.Company=companies.Name WHERE Role="Graphic Designer";
