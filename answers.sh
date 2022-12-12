502a80cd9939940713781cd0f7554c923a2891e6
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-12 18:30:12 +0200

    modify step1

--- answers.sh
+++ answers.sh
@@ -1,43 +1,29 @@
-# first step:
-SELECT name FROM students
-# second step:
+# 1:
+SELECT name FROM students ;
+# 2 :
 select * FROM students WHERE Age>30;
-# third step:
+# 3 :
 SELECT name FROM students WHERE Age=30 AND Gender = 'f';
-# fourh step:
-SELECT Points from students WHERE id = 1
-# step 5:
+# 4 :
+SELECT Points from students WHERE id = 1 ;
+#  5:
 INSERT INTO students (Name, Age, Gender, Points)
-VALUES('yahya', '22', 'M', '600');
-# 6. Increase the points of Basma because she solved a new exercise 
+VALUES('Mouhamad', '23', 'M', '350');
+# 6 :
 UPDATE students
-SET Points='550'
-WHERE id='2';
-# 7.Decrease the points of Alex because he's late today
+SET Points='350'
+WHERE name='Basma';
+# 7 :
 UPDATE students
 SET Points='150'
 WHERE id='1';
-CREATE TABLE Graduates (
-ID Integer Not Null Primary key AUTOINCREMENT,
-Name Text Not Null Unique,
-Age Integer,
-Gender Text,
-Points Integer,
-Graduation Text
+
+CREATE TABLE "Graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER NOT NULL UNIQUE,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
 );
-#creating table
-# 1.Copy Layal's data from students to graduates
-
-INSERT INTO Graduates (Name, Age, Gender, Points)
-SELECT Name, Age, Gender, Points FROM students
-WHERE ID=4; 
-# 2.Add the graduation date previously mentioned to Layal's record in graduates
-
-UPDATE Graduates 
-SET Graduation = '08/09/2018'
-WHERE ID='1';
-
-# 3.Remove Layal's record from students
-
-DELETE FROM students 
-WHERE id='4';
